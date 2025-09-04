05eb58be07e146fdb3410711e70b73002c2421fd
Avengerandy007 <Avengerandy007@users.noreply.github.com>
2025-09-04 16:27:34 +0300

    Made score static

--- src/Update.cpp
+++ src/Update.cpp
@@ -16,15 +16,13 @@ void Reload(){
 }
 
 void UpdateTiles(){
-	Player* player;
         for (uint8_t i = 0; i < 20; i++){
                 for (uint8_t k = 0; k < 20; k++){
 			if (Tile::tileSet.matrix[i][k].m_coinContainer){
 				Tile::tileSet.matrix[i][k].m_coinContainer->Update();
 			}
                         if (Tile::tileSet.matrix[i][k].m_containedEntity){
-				player = Tile::tileSet.matrix[i][k].m_containedEntity->typeId == PLAYER ? dynamic_cast<Player*>(Tile::tileSet.matrix[i][k].m_containedEntity.get()) : nullptr;
-				if (player && (int)player->score == (int)Coin::coinCount){
+				if ((int)Player::score == (int)Coin::coinCount){
 					std::cout << "You win!\n";
 					Reload();
 				}
