2ca949ced37546a44865de2366b71a0c9d243f14
Avengerandy007 <Avengerandy007@users.noreply.github.com>
2025-09-09 10:38:02 +0300

    Fancier way of holding data for levels

--- include/Levels.hpp
+++ include/Levels.hpp
@@ -1,10 +1,18 @@
+#include <array>
 #include <vector>
 #include <string>
 #include "../libpacman/include/FileI.hpp"
 #include "../libpacman/include/Matrix.hpp"
 
+struct Level{
+	uint16_t X, Y;
+	uint16_t coinCount;
+	std::string data;
+	std::string name;
+};
+
 namespace LevelLogic{
-	extern std::vector<std::string> levelData;
+	extern std::vector<Level> levelData;
 	extern void CreateLevels(std::vector<std::string>& list);
 	extern void Reload();
 }
--- src/Levels.cpp
+++ src/Levels.cpp
@@ -1,16 +1,40 @@
 #include "../include/Levels.hpp"
+#include <cstdint>
 #include <filesystem>
 #include <fstream>
+#include <string>
 #include <vector>
 
 namespace LevelLogic{
-	std::vector<std::string> levelData;
-	void GetLevelData(std::vector<std::string>& list){
+	std::vector<Level> levelData;
+	void GetLevelData(std::vector<Level>& list){
 		for (const auto& entry : std::filesystem::directory_iterator("./levels")){
+			//Get file from dir
 			const std::string name = entry.path().string();
 			std::ifstream file(name);
 			if (file.bad()) continue;
-			list.push_back(FileI::FileContent(file));
+			
+			//Create level and assign data to it
+			Level level;
+			level.name = name;
+			level.data = FileI::FileContent(file);
+			//Get coin count of level
+			{
+				uint16_t currentChar = 0;
+				std::string coinCount = "";
+				while(currentChar <= 400){
+					if (level.data[currentChar] == ','){
+						level.data.erase(level.data.begin() + currentChar);
+						break;
+					}
+					coinCount += level.data[currentChar];
+					currentChar++;
+				}
+				level.coinCount = std::stoi(coinCount);
+			}
+			
+
+			list.push_back(level);
 		}
 	}
 
